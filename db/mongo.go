8f3958373f507e39366d1cb3103f7f7c86da68a8
Romain De Oliveira <romainpaulo.deoliveira@gmail.com>
2017-02-18 15:19:37 +0100

    clean code

--- db/mongo.go
+++ db/mongo.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"gopkg.in/mgo.v2"
+	"log"
 )
 
 //Mongo is a db.DB implementation that talks to MongoDB
@@ -14,26 +15,3 @@ type Mongo struct {
 func NewMongo(cl *mgo.Session, db string) Mongo {
 	return Mongo{Session: cl, DatabaseName:db}
 }
-
-/*
-func (r *Mongo) FindAll(result interface{}) error {
-	return r.session.DB(r.databaseName).C(r.collectionName).Find(bson.M{}).All(result)
-}
-
-func (r *Mongo) Find(query interface{}, result interface{}) error {
-	res:=r.session.DB(r.databaseName).C(r.collectionName).Find(query).One(result)
-	log.Print("RES=>"  + res.Error())
-	//return r.session.DB(r.databaseName).C(r.collectionName).Find(query).One(&result)
-	return res
-}
-// Save is the interface implementation
-func (r *Mongo) Save(model interface{}) error {
-	// Write the model to mongo
-	return r.session.DB(r.databaseName).C(r.collectionName).Insert(model)
-}
-
-// Delete is the interface implementation
-func (r *Mongo) Delete(query interface{}) error {
-	return r.session.DB(r.databaseName).C(r.collectionName).Remove(query)
-}
-*/
