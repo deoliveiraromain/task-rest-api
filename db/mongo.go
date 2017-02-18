e97d93d5b99dba36ebb2dbef0e5503777ec69e45
Romain De Oliveira <romainpaulo.deoliveira@gmail.com>
2017-02-18 17:13:33 +0100

    unused log import

--- db/mongo.go
+++ db/mongo.go
@@ -1,17 +1,14 @@
 package db
 
-import (
-	"gopkg.in/mgo.v2"
-	"log"
-)
+import "gopkg.in/mgo.v2"
 
 //Mongo is a db.DB implementation that talks to MongoDB
 type Mongo struct {
-	Session        *mgo.Session
-	DatabaseName   string
+	Session      *mgo.Session
+	DatabaseName string
 }
 
 // NewRedis initializes and returns a redis DB using the given raw redis.v3 client
 func NewMongo(cl *mgo.Session, db string) Mongo {
-	return Mongo{Session: cl, DatabaseName:db}
+	return Mongo{Session: cl, DatabaseName: db}
 }
