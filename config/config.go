00756fa187307fd42f7d12e7a5e884ab42a8f311
Romain De Oliveira <romainpaulo.deoliveira@gmail.com>
2017-02-19 20:23:18 +0100

    adding Dockerfile and config processing

---
+++ config/config.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/kelseyhightower/envconfig"
+)
+
+const (
+	AppName = "task_api"
+)
+
+// Config is the envconfig-compatible configuration struct for this server. See https://github.com/kelseyhightower/envconfig for more detail
+type Config struct {
+	Port      int    `envconfig:"API_PORT" default:"8080"`
+	MongoHost string `envconfig:"MONGO_HOST" default:"localhost"`
+}
+
+// GetConfig uses envconfig to populate and return a Config struct. Returns all envconfig errors if they occurred
+func GetConfig() (*Config, error) {
+	conf := new(Config)
+	if err := envconfig.Process(AppName, conf); err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
